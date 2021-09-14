ab3d49b656850c9453476fffd8702b11bf166834
Lakshmi <challa31@aol.com>
2021-09-13 21:06:02 -0400

    Second commit.

--- primeNumApp/src/main/java/primeNumApp/PrimeNumberController.java
+++ primeNumApp/src/main/java/primeNumApp/PrimeNumberController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+// new line added.
 @Controller
 public class PrimeNumberController {
 
